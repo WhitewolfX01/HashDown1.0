b1bf83c581c7db0859bdf628452ccbb974246c2c
WhitewolfX01 <21cse243.smrutisumanbadjena@giet.edu>
2023-03-08 18:45:53 +0530

    seventh commit

--- hashgenerator/adler32hashgen.py
+++ hashgenerator/adler32hashgen.py
@@ -8,3 +8,4 @@ hash_value = zlib.adler32(input_string.encode())
 
 # Print the hash value
 print("Adler-32 hash of \"{0}\" is :".format(input_string), hex(hash_value))
+print("Note: Ignore the 'x' in the hash value. Only consider the rest 8 characters.")
